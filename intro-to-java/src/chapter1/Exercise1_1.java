4cb9b78e09486c118e38b81de7a48314f776edde
HaiyunJin <jinhaiyun0101@gmail.com>
2016-05-13 09:55:40 -0500

    Start intro to java exercise

---
+++ intro-to-java/src/chapter1/Exercise1_1.java
@@ -0,0 +1,21 @@
+package chapter1;
+
+public class Exercise1_1 {
+	public static void main(String[] args){
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programing is fun");
+		int a=1;
+		int b=2;
+		int c=3;
+		
+		// Exercise 1.3
+		System.out.println(a);
+		//Exercise 1.4
+		for (int i = 1; i <=4;i++){
+			System.out.printf("%5d%5d%5d\n", i,i^2,i^3);			
+		}
+		System.out.println(b);
+
+	}
+}
---
+++ intro-to-java/src/chapter1/Welcome.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+import javax.swing.JOptionPane;
+
+public class Welcome{
+	public static void main(String[] args){
+		// Display Welcome to Java!
+//		JOptionPane.showMessageDialog(null, "Welcome to Java!");
+		JOptionPane.showMessageDialog(null, "Welcome to Java!",
+				"Display Message",JOptionPane.INFORMATION_MESSAGE);
+		
+	}
+}
+
